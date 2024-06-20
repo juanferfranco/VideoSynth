9d6192e34a1e603e80b3fc2d493c1347398456a3
JuanKraken <juanf.franco@upb.edu.co>
2024-06-20 11:58:40 -0500

    add some shapes

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,7 +2,12 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+	ofSetWindowTitle("Video Synth");
+	ofSetWindowShape(1280, 720);
+	ofSetFrameRate(60);
+	ofBackground(ofColor::white);
 
+	ofSetCircleResolution(100);
 }
 
 //--------------------------------------------------------------
@@ -12,12 +17,35 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	ofPushMatrix();
+	ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
+	ofSetColor(ofColor::black);
+	ofDrawRectangle(0, 0, 50, 50);
+	ofSetLineWidth(9.0);
+	ofLine(100, 0, 100, 100);
+	ofSetColor(128);
+	ofTriangle(-100, 0, -200, 0, -150, -100);
+	ofSetColor(ofColor::red);
+	ofCircle(200, -300, 50);
 
+	ofSetColor(0,0,255,255);
+
+	ofPopMatrix();
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+	switch(key) {
+		case ' ':
+			ofSaveFrame();
+			break;
+		case 'f':
+			ofToggleFullscreen();
+			break;
+		case 'q':
+			ofExit();
+			break;
+	}
 }
 
 //--------------------------------------------------------------
